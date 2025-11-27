9d9f1bd1ae93e525d09429bea36b2e7c86c88c15
Lukas <LukasHoltbrink@web.de>
2025-11-27 14:26:26 +0100

    aufgemotzt

--- types/movieAPI.ts
+++ types/movieAPI.ts
@@ -2,8 +2,8 @@ export type MovieAPI = {
   id: number;
   title: string;
   poster_path: string | null;
-  backdrop_path: string | null;
-  overview: string;
-  release_date: string;
-  imdb_id: string | null;
+  imdb_id?: string | null;
+  backdrop_path?: string | null;
+  overview?: string | null;
+  release_date?: string | null;
 };
